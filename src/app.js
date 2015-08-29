9046f7f03f10caf1f75836fc757d319dae4cab17
zeird <l1aqus@gmail.com>
2015-08-29 18:40:28 +0600

    Add second scene and scene toggling

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
+var INITIALIZED = false;
 
 var HelloWorldLayer = cc.Layer.extend({
-    sprite:null,
     ctor:function () {
         //////////////////////////////
         // 1. super init first
@@ -9,65 +9,30 @@ var HelloWorldLayer = cc.Layer.extend({
         // ask the window size
         var size = cc.winSize;
 
-        var big_mine = new cc.Sprite.create(res.big_mine_png);
-        big_mine.setAnchorPoint(cc.p(0.5, 0.5));
-        big_mine.setPosition(cc.p(size.width/2, size.height/2));
-        this.addChild(big_mine, 0);
+        var menuItemFontScene2 = new cc.MenuItemFont("Start", PushScene);
 
-        var move = cc.MoveBy.create(3, cc.p(100, 100));
-        big_mine.runAction(move);
-
-        var infinite_rotate = cc.RepeatForever.create(cc.RotateBy.create(2, 360));
-        big_mine.runAction(infinite_rotate);
-
-        if (cc.sys.capabilities.hasOwnProperty('mouse')) {
-            cc.eventManager.addListener(
-                {
-                    event: cc.EventListener.MOUSE,
-                    onMouseDown: function(event) {
-                        if (event.getButton() === cc.EventMouse.BUTTON_LEFT) {
-                            ResumeMusic();
-                        } else if (event.getButton() === cc.EventMouse.BUTTON_RIGHT) {
-                            PauseMusic();
-                        }
-                    },
-                },
-                this
-            );
-        }
+        var menu = new cc.Menu(menuItemFontScene2);
+        menu.alignItemsVertically();
+        this.addChild(menu);
 
         cc.audioEngine.playEffect(res.app_start_sound); //returns soundEffect id
-        cc.audioEngine.playMusic(res.music, true);
-        cc.audioEngine.setMusicVolume(0.25);
-        PauseMusic();
-        //cc.audioEngine.setEffectsVolume(0-1)
-        //cc.audioEngine.stopAllEffects()
-        //cc.audioEngine.stopEffect(sound_id)
-        //cc.audioEngine.playMusic(musicRes, repeat)
-        //cc.audioEngine.stopMusic()
-        //this.scheduleOnce(func, seconds)
 
         return true;
     }
 });
 
-var PauseMusic = function() {
-    cc.audioEngine.pauseMusic();
-};
-
-var ResumeMusic = function() {
-    cc.audioEngine.resumeMusic();
-};
-
-var StopMusic = function() {
-    cc.audioEngine.stopMusic();
+var PushScene = function() {
+    var scene = new HelloWorldScene2();
+    cc.director.pushScene(scene);
 };
 
 var HelloWorldScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new HelloWorldLayer();
-        this.addChild(layer);
+        if (!INITIALIZED) {
+            INITIALIZED = true;
+            var layer = new HelloWorldLayer();
+            this.addChild(layer);
+        }
     }
 });
-
