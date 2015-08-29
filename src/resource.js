430e1e9c744b6f89936d3944dbdb66c11d6f2c52
zeird <l1aqus@gmail.com>
2015-08-29 22:10:32 +0600

    Add 1, 1x, 2 and 2x resources

--- src/resource.js
+++ src/resource.js
@@ -2,6 +2,10 @@ var res = {
     big_mine_png: "res/big_mine.png",
     app_start_sound: "res/app_start.mp3",
     music: "res/MainMusic.ogg",
+    one: "res/1.png",
+    onex: "res/1x.png",
+    two: "res/2.png",
+    twox: "res/2x.png",
 };
 
 var g_resources = [];
