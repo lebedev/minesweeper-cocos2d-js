6e7521caf9d12062ba1b01b1add570ab7263c606
zeird <l1aqus@gmail.com>
2015-08-29 22:12:03 +0600

    Add a button that changes texture

--- src/app.js
+++ src/app.js
@@ -13,12 +13,32 @@ var HelloWorldLayer = cc.Layer.extend({
 
         var menu = new cc.Menu(menuItemFontScene2);
         menu.alignItemsVertically();
-        this.addChild(menu);
+        //this.addChild(menu);
+
+        var button = new ccui.Button();
+        button.loadTextures(res.one, res.onex);
+        button.x = size.width/2;
+        button.y = size.height/2;
+        button.addTouchEventListener(this.touchEvent, this);
+        this.addChild(button);
 
         cc.audioEngine.playEffect(res.app_start_sound); //returns soundEffect id
 
+        //this.schedule(this.functionCallback);
+
         return true;
+    },
+    touchEvent: function(sender, type) {
+        switch(type) {
+        case ccui.Widget.TOUCH_BEGAN: cc.log(sender); break;
+        case ccui.Widget.TOUCH_MOVED: cc.log('MOVED'); break;
+        case ccui.Widget.TOUCH_ENDED: cc.log('ENDED'); sender.loadTextures(res.two, res.twox); this.scheduleOnce(PushScene, 1); break;
+        case ccui.Widget.TOUCH_CANCELLED: cc.log('CANCELLED'); break;
+        }
     }
+    //this.functionCallback:function() {
+    //
+    //}
 });
 
 var PushScene = function() {
